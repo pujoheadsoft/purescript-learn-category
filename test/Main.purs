f70b7e1bfabbde8a4a57d32c582bb7da61abd7c6
Pujo <pujoheadsoft@gmail.com>
2024-03-17 00:57:06 +0900

    test追加

--- test/Main.purs
+++ test/Main.purs
@@ -4,7 +4,8 @@ import Prelude
 
 import Data.Array (head)
 import Data.Maybe (Maybe(..))
-import Data.String (null)
+import Data.Number (isNaN)
+import Data.String (length, null)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Test.Spec (describe, it)
@@ -14,33 +15,33 @@ import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [consoleReporter] do
-  -- describe "Categoryは以下の条件を満たす" do
-  --   let 
-  --     f = isNaN
-  --     g = show
-  --     h = length
-  --     a = 100.0
-  --   it "結合律を満たす" do
-  --     ((h <<< g) <<< f) a `shouldEqual` (h <<< (g <<< f)) a
+  describe "Categoryは以下の条件を満たす" do
+    let 
+      f = isNaN
+      g = show
+      h = length
+      a = 100.0
+    it "結合律を満たす" do
+      ((h <<< g) <<< f) a `shouldEqual` (h <<< (g <<< f)) a
 
-  --   it "左単位律を満たす" do
-  --     (identity <<< f) a `shouldEqual` f a
+    it "左単位律を満たす" do
+      (identity <<< f) a `shouldEqual` f a
 
-  --   it "右単位律を満たす" do
-  --     f a `shouldEqual` (f <<< identity) a
+    it "右単位律を満たす" do
+      f a `shouldEqual` (f <<< identity) a
 
 
-  -- describe "Maybe NumberはFunctor則を満たす" do
-  --   it "射の合成を保つ" do
-  --     let 
-  --       a = Just 1.0
-  --       f = isNaN
-  --       g = show
-  --     (map (g <<< f) a) `shouldEqual` ((map g <<< map f) a)
+  describe "Maybe NumberはFunctor則を満たす" do
+    it "射の合成を保つ" do
+      let 
+        a = Just 1.0
+        f = isNaN
+        g = show
+      (map (g <<< f) a) `shouldEqual` ((map g <<< map f) a)
     
-  --   it "恒等射を保つ" do
-  --     let a = Just 1.0
-  --     map identity a `shouldEqual` identity a
+    it "恒等射を保つ" do
+      let a = Just 1.0
+      map identity a `shouldEqual` identity a
 
   describe "自然変換は可換である" do
     let
@@ -52,7 +53,7 @@ main = launchAff_ $ runSpec [consoleReporter] do
     it "先頭が空文字でない場合" do
       nt_map ["any", ""] `shouldEqual` map_nt ["any", ""]
 
-  describe "Array IntはMonad則を満たす" do
+  describe "Array Intはモナドの条件を満たす" do
     it "結合律を満たす" do
       let 
         t3 = [
@@ -64,26 +65,46 @@ main = launchAff_ $ runSpec [consoleReporter] do
             [7, 8]
           ]
         ]
-        {-
-          Aは対象
-          fは関数
-          関手Tは対象関数と射関数からなる
-          T(A)は対象関数で写した対象
-          T(f)は射関数(map)で写した関数
-          Aを配列[]とする。
-          T(A)は配列の配列[[]]となる
-          Aが配列なのでfは配列に対しての関数となる
-          T(f)は map 配列の関数 となる。
+      (join <<< map join) t3 `shouldEqual` (join <<< join) t3
 
-          μはjoin
-          μ_Aは配列のjoin
-          T(μ_A)は、μ_Aが関数なので射関数になり、(map 配列のjoin)となる。
+    it "単位元律を満たす" do
+      let
+        ta = [10, 20, 30]
+      (join <<< pure) ta `shouldEqual` identity ta
+      (join <<< map pure) ta `shouldEqual` identity ta
 
-          T(A)は配列の配列[[]]だったので、
-          μ_T(A)は、配列の配列のjoin
-        -}
-        μ = join
-        tμ_a = map μ
-        μ_ta = μ
-      (μ <<< tμ_a) t3 `shouldEqual` (μ <<< μ_ta) t3
+  describe "Maybeはモナド則を満たす" do
+    it "左単位元律を満たす" do
+      -- `pure a >>= h = h a`
+      let h = Just
+      (pure 10 >>= h) `shouldEqual` h 10
+    
+    it "右単位元律を満たす" do
+      -- `m >>= pure = m`
+      let m = Just 10
+      (m >>= pure) `shouldEqual` m
+    
+    it "結合律を満たす" do
+      -- (m >>= g) >>= h = m >>= (\x -> g x >>= h)
+      let
+        m = Just 1
+        g = \x -> pure (x + 10)
+        h = \x -> pure (show x)
+      ((m >>= g) >>= h) `shouldEqual` (m >>= (\x -> g x >>= h))
+
+  describe "Maybeはモナド則を満たす(join版)" do
+    it "左単位元律を満たす" do
+      let h = Just
+      (join <<< map h) (pure 10) `shouldEqual` h 10
+    
+    it "右単位元律を満たす" do
+      let m = Just 10
+      (join <<< map pure) m `shouldEqual` m
+    
+    it "結合律を満たす" do
+      let
+        m = Just 1
+        g = \x -> pure (x + 10)
+        h = \x -> pure (show x)
+      ((join <<< map h) <<< (join <<< map g)) m `shouldEqual` (join <<< map (\x -> (join <<< map h) (g x))) m
 
